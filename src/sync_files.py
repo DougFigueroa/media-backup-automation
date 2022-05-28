388a103b8d480d753274939f3052bae7af628039
Douglas Figueroa <douglasr.figueroa@gmail.com>
2022-05-28 01:00:58 -0400

    Adds first version of the sync process Reads the configuration file and imports the basic stuff

---
+++ src/sync_files.py
@@ -0,0 +1,27 @@
+import configparser
+import os
+from dirsync import sync
+
+
+def read_config(file_path):
+    parser = configparser.ConfigParser()
+    parser.read(file_path)
+    return parser
+
+
+def sync_files(source, destionation):
+    return
+
+
+def handler():
+    current_dir = os.path.dirname(__file__)
+    config_file_path = os.path.join(current_dir, 'cfg', 'config.cfg')
+    config = read_config(config_file_path)
+    source = config.get('folder_config', 'source_path')
+    destination = config.get('folder_config', 'destination_path')
+    sync_files(source, destionation)
+    print('>>>>>  Sync completed :)  <<<<<')
+
+
+if __name__ == "__main__":
+    handler()
